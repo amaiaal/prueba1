513e70159455112355c12062fc4506e4bb842839
in1dw3 <in1dw3@HZ352019.elorrieta.int>
2023-01-20 16:38:18 +0100

    Importante poner los cambios realizados
    "Cambio tipos de datos y bucle suma"

---
+++ junit/src/calculadora/Calculadora.java
@@ -0,0 +1,28 @@
+package calculadora;
+
+public class Calculadora {
+
+	private int num1 = 0; 
+	private int num2 = 0;
+	
+	public Calculadora (int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int suma () {
+		return num1 + num2; 
+	}
+	
+	public int resta () {
+		return num1 - num2; 
+	}
+	
+	public int multiplica () {
+		return num1 * num2; 
+	}
+	
+	public int divide () {
+		return num1 / num2; 
+	}
+}
