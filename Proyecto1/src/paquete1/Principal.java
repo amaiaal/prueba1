60a02c42d4734bf78ebb5a6c462846cfa318d6ce
in1dw3 <in1dw3@HZ352019.elorrieta.int>
2023-01-27 15:42:23 +0100

    f

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,6 +3,7 @@ package paquete1;
 public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Estamos empezando el proyecto con Git");
+		System.out.println("1ª modificación del proyecto");
 	}
 
 }
